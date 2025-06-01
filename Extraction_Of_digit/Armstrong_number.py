398a11447b1f34822d4056afcca38ad99c72cf2b
AbdullahRFA <shakibrybmn@gmail.com>
2025-06-01 18:26:24 +0600

    added a problem that check if a given number is armstrong or not

---
+++ Extraction_Of_digit/Armstrong_number.py
@@ -0,0 +1,28 @@
+"""
+
+"""
+
+def count_of_digit(n):
+    num = n
+    cnt = 0
+    while num != 0:
+        num //= 10
+        cnt += 1
+    return cnt
+def check_armstrong(n,cnt):
+    num = n
+    armstrong_number = 0
+    while num != 0:
+        r = num % 10
+        armstrong_number += r**cnt
+        num //= 10
+    return n == armstrong_number
+
+
+n = int(input("Enter a number to check if it is armstrong number or not: "))
+cnt = count_of_digit(n)
+if check_armstrong(n,cnt):
+    print(f"{n} is an armstrong number")
+else:
+    print(f"{n} is not an armstrong number")
+
