e28bc567d5808d1db52a064bb018207d37d2df43
AbdullahRFA <shakibrybmn@gmail.com>
2025-08-17 00:48:18 +0600

    modified for right shift operator

--- Bit Manipulation/check_if_ith_bit_is_set_or_not.py
+++ Bit Manipulation/check_if_ith_bit_is_set_or_not.py
@@ -1,11 +1,24 @@
 num = int(input("Enter a number to check if it's ith bit is set or not : "))
 ith = int(input("Enter the value of 'i' : "))
 
-tem = 1<<ith
+# tem = 1<<ith
+#
+# result = num & tem
+#
+# if result:
+#     print(f"{ith}th bit of {num}({bin(num)[2:]} is set")
+# else:
+#     print(f"{ith}th bit of {num}({bin(num)[2:]} is not set")
 
-result = num & tem
+# if num & (1<<ith) :
+#     print(f"{ith}th bit of {num}({bin(num)[2:]}) is set")
+# else:
+#     print(f"{ith}th bit of {num}({bin(num)[2:]}) is not set")
 
-if result:
-    print(f"{ith}th bit of {num}({bin(num)[2:]} is set:)")
+#   Using right shift
+
+if (num>>ith)&1 :
+    print(f"{ith}th bit of {num}({bin(num)[2:]}) is set")
 else:
-    print(f"{ith}th bit of {num}({bin(num)[2:]} is not set:)")
+    print(f"{ith}th bit of {num}({bin(num)[2:]}) is not set")
+
