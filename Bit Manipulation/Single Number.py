7c60506a4baa7a8d63a58fbfcc2b1c4811314889
AbdullahRFA <shakibrybmn@gmail.com>
2025-08-19 00:54:44 +0600

    Given a non-empty array of integers nums, every element appears twice except for one. Find that single one.

---
+++ Bit Manipulation/Single Number.py
@@ -0,0 +1,41 @@
+"""
+Given a non-empty array of integers nums, every element appears twice except for one. Find that single one.
+
+You must implement a solution with a linear runtime complexity and use only constant extra space.
+
+
+
+Example 1:
+
+Input: nums = [2,2,1]
+
+Output: 1
+
+Example 2:
+
+Input: nums = [4,1,2,1,2]
+
+Output: 4
+
+Example 3:
+
+Input: nums = [1]
+
+Output: 1
+
+
+"""
+from typing import  List
+
+class Solution:
+    def singleNumber(self, nums: List[int]) -> int:
+        res = nums[0]
+
+        for i in range(1,len(nums)):
+            res^= nums[i]
+        return res
+
+nums = [1,1,2,3,3,4,4,5,5]
+
+slv = Solution()
+print(slv.singleNumber(nums))
