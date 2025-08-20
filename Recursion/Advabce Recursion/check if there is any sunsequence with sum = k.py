f14399c86bb8bfe451e9b88ddd8365cbd4a22b14
AbdullahRFA <shakibrybmn@gmail.com>
2025-08-20 12:36:49 +0600

    added a problem that  check whether there exists a subsequence such that the sum of all elements in the subsequence equals the given target sum(k).

---
+++ Recursion/Advabce Recursion/check if there is any sunsequence with sum = k.py
@@ -0,0 +1,35 @@
+nums = list(map(int, input("Enter element separate by space : ").split()))
+k = int(input("Enter the value of 'K' : "))
+result = []
+
+
+def Solution(idx, total, subset, k):
+    if total == k:
+        result.append(subset.copy())
+        return True
+    elif total > k:
+        return False
+    elif idx >= len(nums):
+        return False
+
+    # include nums[idx]
+    subset.append(nums[idx])
+    SUM = total + nums[idx]
+    # Solution(idx + 1, total + nums[idx], subset, k)
+    pick = Solution(idx + 1, SUM, subset, k)
+    if pick:
+        return True
+    # subset.pop()
+    e = subset.pop()
+    SUM -= e
+
+    # exclude nums[idx]
+    # Solution(idx + 1, total, subset, k)
+    Not_pick = Solution(idx + 1, SUM, subset, k)
+    return Not_pick
+
+
+subset = []
+print(Solution(0, 0, subset, k))
+# print(result)
+# print(f"Total subsequences equals=={k} is {len(result)}")
