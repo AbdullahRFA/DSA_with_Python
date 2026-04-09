a8d87000f6267eda64a7ef0280970efbaef2f8f4
AbdullahRFA <shakibrybmn@gmail.com>
2026-04-09 22:10:51 +0600

    explain the problem and it's solution approch

--- Binary Tree/level_order_traversal.py
+++ Binary Tree/level_order_traversal.py
@@ -1,3 +1,15 @@
+'''
+Level Order Traversal of a binary tree is a breadth-first traversal method where we visit all the nodes at the present depth level before moving on to the nodes at the next depth level. This can be implemented using a queue data structure to keep track of the nodes to be visited.
+In the provided code, we define a class `CreateTree` to represent each node in the binary tree. The `level_order_traversal` function takes the root of the tree as input and uses a queue to perform the level order traversal. It prints the data of each node as it is visited.
+The example at the end of the code creates a binary tree with 7 nodes and calls the `level_order_traversal` function to print the nodes in level order. The expected output will be:
+```Level Order Traversal of binary tree is:
+1 2 3 4 5 6 7 ```
+
+time complexity: O(n) where n is the number of nodes in the binary tree, as we visit each node exactly once.
+space complexity: O(n) in the worst case, when the binary tree is completely unbalanced (like a linked list), the queue can hold all the nodes at once. In the best case, when the tree is perfectly balanced, the space complexity is O(w) where w is the maximum width of the tree (the maximum number of nodes at any level).
+'''
+
+
 from collections import deque
 
 
